e9e3041a58bef4fa2b714a87a61f846fde5da82c
Mark Glushko <m.glushko@wnfx.ru>
2023-01-25 18:52:52 +0300

    added SplashFragment.kt +  BaseFragment.kt + navigation resource

---
+++ app/src/main/java/com/glushko/testappbarrier/presentation/splash/SplashFragment.kt
@@ -0,0 +1,10 @@
+package com.glushko.testappbarrier.presentation.splash
+
+import androidx.fragment.app.Fragment
+import com.glushko.testappbarrier.R
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+
+
+}
